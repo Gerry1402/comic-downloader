e04353df83f7af27289fecdcefd7edee7ff46517
Gerry1402 <gerard.vello@gmail.com>
2026-04-23 19:58:42 +0200

    ♻️ refactor: Simplify get_data_as_dict function
    Removed the unused create_json parameter and related code from get_data_as_dict function to streamline its functionality.
    
    * Eliminated the create_json parameter as it was not utilized.
    * Removed commented-out json_dump call to clean up the code.
    * Aimed to enhance code readability and maintainability.

--- data/data.py
+++ data/data.py
@@ -2,12 +2,10 @@ from pathlib import Path
 
 import pandas as pd
 
-# from utils.utils import json_dump
-
 data_path = Path(__file__).parent / "data.xlsx"
 
 
-def get_data_as_dict(create_json: bool = False) -> dict:
+def get_data_as_dict() -> dict:
     df = pd.read_excel(data_path.with_suffix(".xlsx"), header=None)
     sources = {name.split()[0]: i for i, name in enumerate(df.iloc[0].tolist()[:-1])}
     data = {}
@@ -17,9 +15,6 @@ def get_data_as_dict(create_json: bool = False) -> dict:
         is_completed: str = row.iloc[-1]
         index = sources[source]
         data[row.iloc[index - 1]] = (row.iloc[index], source, is_completed)
-
-    # if create_json:
-    #     json_dump(data, data_path.with_suffix(".json"))
 
     return data
 
